dc466d38e21395a552dab9da28f8373b13093dac
host <2257699870@qq.com>
2024-03-30 12:27:41 +0800

    none

---
+++ questionScrolling/vueDev/exam/src/App.vue
@@ -0,0 +1,5 @@
+<script src='./assets/js/exam.js'></script>
+
+<template></template>
+
+<style src='./assets/css/exam.css'></style>
---
+++ questionScrolling/vueDev/exam/src/main.js
@@ -0,0 +1,6 @@
+import './assets/css/exam.css'
+
+import { createApp } from 'vue'
+import App from './App.vue'
+
+createApp(App).mount('#app')
