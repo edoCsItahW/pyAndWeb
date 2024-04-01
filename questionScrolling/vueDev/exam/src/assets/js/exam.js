7a5020808d99c08414e32e0d01d748ae9513b07d
host <2257699870@qq.com>
2024-04-01 20:49:22 +0800

    none

--- questionScrolling/vueDev/exam/src/assets/js/exam.js
+++ questionScrolling/vueDev/exam/src/assets/js/exam.js
@@ -22,6 +22,8 @@ class handleData {
         this._key = data["key"]
 
         this.topDiv = elemOption._createNewElement($.id("app"), "div", null, "container")
+
+        this.optionArray = ["A", "B", "C", "D"]
     }
 
     get data() {return this._data}
@@ -58,27 +60,27 @@ class handleData {
             this.data["sChoice"].forEach(dict => {
                 const quesDiv = elemOption._createNewElement(sChoiceDiv, "div")
 
-                elemOption._createNewElement(quesDiv, "blockquote", `${dict["chapter"]}: ${dict["note"]}`)
+                elemOption._createNewElement(quesDiv, "blockquote", `${dict["chapter"]}`)
 
                 elemOption._createNewElement(quesDiv, "p", `(${this.key["sChoice"]})${dict["question"]}`)
 
-                const from = elemOption._createNewElement(quesDiv, "from")
+                const from = elemOption._createNewElement(quesDiv, "from", null, "optionfrom")
 
-                dict["option"].forEach(str => {
-                    const hash = $.toHash(str)
+                dict["option"].forEach((str, i) => {
+                    // const hash = $.toHash(str)
 
-                    const optionDiv = elemOption._createNewElement(from, "div")
+                    const optionDiv = elemOption._createNewElement(from, "div", `${this.optionArray[i]}.${str}`, "option")
 
-                    const input = elemOption._createNewElement(optionDiv, "input", null, null, hash, {type: "checkbox"})
-                    elemOption._createNewElement(optionDiv, "label", str, null, null, {for: hash})
+                    // const input = elemOption._createNewElement(optionDiv, "input", null, null, hash, {type: "checkbox"})
+                    // elemOption._createNewElement(optionDiv, "label", str, null, null, {for: hash})
 
-                    elemArray.push(input)
+                    elemArray.push(optionDiv)
                 })
 
 
             })
 
-            elemOption.elementRepel(elemArray)
+            elemOption.elementRepel(elemArray, "click", "classList", "[option choice]")
 
         }
     }
@@ -90,19 +92,27 @@ class handleData {
             this.data["multChoice"].forEach(dict => {
                 const quesDiv = elemOption._createNewElement(sChoiceDiv, "div")
 
-                elemOption._createNewElement(quesDiv, "blockquote", `${dict["chapter"]}: ${dict["note"]}`)
+                elemOption._createNewElement(quesDiv, "blockquote", `${dict["chapter"]}`)
 
                 elemOption._createNewElement(quesDiv, "p", `(${this.key["multChoice"]})${dict["question"]}`)
 
-                const from = elemOption._createNewElement(quesDiv, "from")
+                const from = elemOption._createNewElement(quesDiv, "from", null, "optionfrom")
 
-                dict["option"].forEach(str => {
-                    const hash = $.toHash(str)
+                dict["option"].forEach((str, i) => {
+                    // const hash = $.toHash(str)
 
-                    const optionDiv = elemOption._createNewElement(from, "div")
+                    const optionDiv = elemOption._createNewElement(from, "div", `${this.optionArray[i]}.${str}`, "option")
 
-                    elemOption._createNewElement(optionDiv, "input", null, null, hash, {type: "checkbox"})
-                    elemOption._createNewElement(optionDiv, "label", str, null, null, {for: hash})
+                    optionDiv.addEventListener("click", ev => {
+                        if (Array.from(ev.target.classList).includes("choice")) {
+                            ev.target.classList.remove("choice")
+                        }
+                        else {
+                            ev.target.classList.add("choice")
+                        }})
+
+                    // elemOption._createNewElement(optionDiv, "input", null, null, hash, {type: "checkbox"})
+                    // elemOption._createNewElement(optionDiv, "label", str, null, null, {for: hash})
 
                 })
             })
@@ -110,7 +120,19 @@ class handleData {
     }
 
     anQuestionDo() {
+        if ("anQuestion" in this.data) {
+            const anQuestionDiv = elemOption._createNewElement(this.topDiv, "div", null, "quesblock")
 
+            this.data["anQuestion"].forEach(dict => {
+                const quesDiv = elemOption._createNewElement(anQuestionDiv, "div", null, "anques")
+
+                elemOption._createNewElement(quesDiv, "blockquote", `${dict["chapter"]}`)
+
+                elemOption._createNewElement(quesDiv, "p", `(${this.key["anQuestion"]})${dict["question"]}`)
+
+                elemOption._createNewElement(quesDiv, "textarea", null, "textinput")
+            })
+        }
     }
 
     beginSpawn() {
@@ -118,6 +140,8 @@ class handleData {
         this.sChoiceDo()
 
         this.multChoiceDo()
+
+        this.anQuestionDo()
     }
 
 }
